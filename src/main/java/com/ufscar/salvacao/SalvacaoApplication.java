868e75f4c96c5f6d026e587a90d02aed2f9f9123
Leo Campelo <leocampelo16@gmail.com>
2021-05-07 09:42:03 -0300

    dll-auto validate

--- src/main/java/com/ufscar/salvacao/SalvacaoApplication.java
+++ src/main/java/com/ufscar/salvacao/SalvacaoApplication.java
@@ -2,12 +2,14 @@ package com.ufscar.salvacao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class SalvacaoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SalvacaoApplication.class, args);
+		//System.out.println(new BCryptPasswordEncoder().encode("admin"));
 	}
 
 }
