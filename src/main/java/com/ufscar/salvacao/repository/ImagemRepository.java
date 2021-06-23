ef053d0684828f39c1ed77a45a350381a729bc63
Leo <leocampelo16@gmail.com>
2021-06-23 18:05:21 -0300

    Arrumando coisas

--- src/main/java/com/ufscar/salvacao/repository/ImagemRepository.java
+++ src/main/java/com/ufscar/salvacao/repository/ImagemRepository.java
@@ -3,10 +3,9 @@ package com.ufscar.salvacao.repository;
 import com.ufscar.salvacao.model.Imagem;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ImagemRepository extends JpaRepository<Imagem, Integer> {
     
-    @Query(value = "select img from FOTOS img where img.url = ?1")
-    Imagem findByURL(String url);
 }
